b0f0b572f359e1dc538d6cad3d4b55a69c204087
Pavel Afanasyev <afanasyevpv9@gmail.com>
2022-06-21 16:43:54 +0300

    Initial Commit

---
+++ TaskForInterectiveVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskForInterectiveVideo
+//
+//  Created by Павел Афанасьев on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
